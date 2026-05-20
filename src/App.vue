9f30e19b4c57dc4c603c955596f7000856a42a51
dani <dhannizd2@gmail.com>
2026-05-20 08:45:27 +0700

    page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <DecisionTree />
 </template>
+
+<script setup>
+import DecisionTree from "./components/DecisionTree.vue";
+</script>
